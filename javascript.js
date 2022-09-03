4a0c13d4567054816253ea075a9584828912c836
rqpt <1ernstvermeulen1@gmail.com>
2022-09-03 15:59:10 +0200

    Complete loop for a best of 5 game

--- javascript.js
+++ javascript.js
@@ -1,28 +1,57 @@
-const computerSelection = getComputerSelection();
-const playerSelection = prompt("Choose your weapon");
+let roundResult;
 
-let result = playRound(playerSelection, computerSelection);
-console.log(result);
+// Declare a function called game.
+// Declare counter variables called playerPoints, computerPoints 
+// loop the following five times-
+// call the playRound function
+// if playRound is equal to "You've won!", increment playerPoints by 1
+// else if playRound is equal to "You've lost :(", increment computerPoints by 1
+
+// if playerPoints is a greater than computerPoints, console log "VICTORY"
+// else if playerPoints equals computerPoints, console log "TIE"
+// else if playerPoints is a lesser than computerPoints, console log "DEFEAT"
+//
+function game() {
+	let playerPoints = 0;
+	let computerPoints = 0;
+
+	for(let i = 0; i < 5; i++) {
+		playRound();
+		if (roundResult == "You've won!") {
+			++playerPoints;
+		} else if (roundResult == "You've lost :(") {
+			++computerPoints;
+		}
+		console.log(playerPoints);
+		console.log(computerPoints);
+	}
+	if (playerPoints > computerPoints) {
+		console.log("VICTORY");
+	} else if (playerPoints == computerPoints) {
+		console.log("TIE");
+	} else {
+		console.log("DEFEAT");
+	}
+}
+
+function playRound() {
+	const playerSelection = prompt("Choose your weapon!").toUpperCase();
+	const computerSelection = getComputerSelection();
+		console.log(playerSelection);
+		console.log(computerSelection);
+	if (playerSelection === computerSelection) {
+		roundResult = "Tied";
+	} else if ((playerSelection === "ROCK" && computerSelection === "SCISSORS") || (playerSelection === "PAPER" && computerSelection === "ROCK") || (playerSelection === "SCISSORS" && computerSelection === "PAPER")) {
+		roundResult = "You've won!";
+	} else {
+		roundResult = "You've lost :(";
+	}
+}
 
 function getComputerSelection() {
-	let possibleSelections = ["Rock", "Paper", "Scissors"];
+	let possibleSelections = ["ROCK", "PAPER", "SCISSORS"];
 	function getRandomInt() {
 		return Math.floor(Math.random() * 3);
 	}
 	return possibleSelections[getRandomInt()];
 }
-
-function playRound(playerSelection, computerSelection) {
-	playerSelection = playerSelection.toUpperCase();
-	computerSelection = computerSelection.toUpperCase();
-	if (playerSelection === computerSelection) {
-		return "Tied";
-	} else if ((playerSelection === "ROCK" && computerSelection === "SCISSORS") || (playerSelection === "PAPER" && computerSelection === "ROCK") || (playerSelection === "SCISSORS" && computerSelection === "PAPER")) {
-		return "You've won!";
-	} else {
-		return "You've lost :(";
-	}
-}
-
-console.log(playerSelection);
-console.log(computerSelection);
