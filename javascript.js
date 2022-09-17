421f9339a4b4ad36a7f3eb3ab4e2b984fcff80a0
rqpt <1ernstvermeulen1@gmail.com>
2022-09-17 17:52:11 +0200

    Finalised first to 5 logic for rps game

--- javascript.js
+++ javascript.js
@@ -1,41 +1,56 @@
-// Play the game by clicking on buttons on the screen.
-
-const buttons = document.querySelectorAll('button');
-buttons.forEach(button => button.addEventListener('click', playRound));
-
 const resultsContainer = document.querySelector('.results-container');
 
-const playerPara = document.createElement('p');
-const computerPara = document.createElement('p');
-const playerScore = document.createElement('p');
-const computerScore = document.createElement('p');
-const results = document.createElement('p');
-resultsContainer.appendChild(playerPara);
-resultsContainer.appendChild(computerPara);
-resultsContainer.appendChild(playerScore);
-resultsContainer.appendChild(computerScore);
-resultsContainer.appendChild(results);
+const pParaDisplay = document.createElement('p');
+const pScoreDisplay = document.createElement('p');
+const cParaDisplay = document.createElement('p');
+const cScoreDisplay = document.createElement('p');
+const resultDisplay = document.createElement('p');
+resultsContainer.appendChild(pParaDisplay);
+resultsContainer.appendChild(cParaDisplay);
+resultsContainer.appendChild(pScoreDisplay);
+resultsContainer.appendChild(cScoreDisplay);
+resultsContainer.appendChild(resultDisplay);
 
-function playGame() {
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => button.addEventListener('click', play));
+let pScore = 0;
+let cScore = 0;
 
-}
+function play() {
+	const pSelection = this.innerText.toUpperCase();
+	const cSelection = getCSelection();
+	pParaDisplay.innerText = `Player choses ${pSelection}.`;
+	cParaDisplay.innerText = `Computer choses ${cSelection}.`;
 
-function playRound() {
-	const playerSelection = this.innerText.toUpperCase();
-	const computerSelection = getComputerSelection();
-  playerPara.innerText = `Player choses ${playerSelection}.`;
-  computerPara.innerText = `Computer choses ${computerSelection}.`;
-
-	if (playerSelection === computerSelection) {
-		results.innerText = "Tied";
-	} else if ((playerSelection === "ROCK" && computerSelection === "SCISSORS") || (playerSelection === "PAPER" && computerSelection === "ROCK") || (playerSelection === "SCISSORS" && computerSelection === "PAPER")) {
-		results.innerText = "You've won!";
+	if (pSelection === cSelection) {
+		return;
+	} else if	((pSelection === "ROCK" && cSelection === "SCISSORS") ||
+				 		 (pSelection === "PAPER" && cSelection === "ROCK") ||
+					(pSelection === "SCISSORS" && cSelection === "PAPER")) {
+		++pScore;
+		pScoreDisplay.innerText = `Player score = ${pScore}`;
+		cScoreDisplay.innerText = `Computer score = ${cScore}`;
 	} else {
-		results.innerText = "You've lost :(";
+		++cScore;
+		pScoreDisplay.innerText = `Player score = ${pScore}`;
+		cScoreDisplay.innerText = `Computer score = ${cScore}`;
 	}
-}
+	if (pScore === 5) {
+		alert("VICTORY");
+		pScore = 0;
+		cScore = 0;
+		pScoreDisplay.innerText = `Player score = ${pScore}`;
+		cScoreDisplay.innerText = `Computer score = ${cScore}`;
+	} else if (cScore === 5) {
+		alert("DEFEAT");
+		pScore = 0;
+		cScore = 0;
+		pScoreDisplay.innerText = `Player score = ${pScore}`;
+		cScoreDisplay.innerText = `Computer score = ${cScore}`;
+	}
+};
 
-function getComputerSelection() {
+function getCSelection() {
 	let possibleSelections = ["ROCK", "PAPER", "SCISSORS"];
 	function getRandomInt() {
 		return Math.floor(Math.random() * 3);
